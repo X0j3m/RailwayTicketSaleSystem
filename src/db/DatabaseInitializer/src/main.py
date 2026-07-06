55fd4191974d31b0f465806c12665baab0896114
Michał Ptasznik <michal.ptasznik@protonmail.com>
2026-07-06 19:31:04 +0200

    updated main.py

--- src/db/DatabaseInitializer/src/main.py
+++ src/db/DatabaseInitializer/src/main.py
@@ -1,19 +1,20 @@
 from neo.schedule.db_insert import insert_schedule
-from sql.db_handler import DatabaseConnection
+from sql.db_handler import DatabaseConnection as SqlDatabaseConnection
+from neo.db_handler import DatabaseConnection as Neo4jDatabaseConnection
 from sql.fleet.db_insert import insert_fleet
 
 
 def main():
-    sql_dbc = DatabaseConnection(
+    sql_dbc = SqlDatabaseConnection(
         ip="127.0.0.1",
         port="1433",
         password="RootP@ssword123",
     )
     insert_fleet(sql_dbc)
 
-    neo4j_dbc = DatabaseConnection(
+    neo4j_dbc = Neo4jDatabaseConnection(
         ip="127.0.0.1",
-        port="1433"
+        port="7687"
     )
     insert_schedule(neo4j_dbc)
 
